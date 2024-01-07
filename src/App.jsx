bc447a37101ea4ca9a8dfb51e4f907deb2bec2f4
Wadad Parker <wadadparker5@gmail.com>
2024-01-07 10:19:07 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Route,Routes } from "react-router-dom"
+
+function App() {
+
+  return (
+      <div>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+        </Routes>
+    </div>
+  )
+}
+
+export default App
